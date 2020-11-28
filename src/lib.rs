9e4dabb948edb9ae0594e013a1318bac4b886377
Eduardo Tongson <propolice@gmail.com>
2020-11-28 20:01:58 +0800

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+use std::io::Read;
+
+extern crate ureq;
+extern crate serde_cbor;
+use std::collections::BTreeMap;
+use serde_cbor::{from_slice};
+
+#[no_mangle]
+pub extern "C" fn get(c: *const c_char) -> *const c_char {
+  let b = unsafe { CStr::from_ptr(c).to_bytes() };
+  let v: BTreeMap<String, String> = from_slice(b).unwrap();
+  let resp = ureq::get(&v["url"]).call();
+
+  let mut bytes = vec![];
+  if resp.status().to_string() == "200" {
+    let mut reader = resp.into_reader();
+    reader.read_to_end(&mut bytes);
+  } else {
+    bytes = resp.status().to_string().as_bytes().to_vec();
+  }
+
+  let c_str = CString::new(bytes).unwrap();
+  let ptr = c_str.as_ptr();
+  std::mem::forget(c_str);
+  return ptr
+}
+
+
+
+
