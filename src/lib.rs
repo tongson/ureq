6220211e4e1748e4194cc9b9ec738908a61393b8
Eduardo Tongson <propolice@gmail.com>
2020-11-29 13:14:45 +0800

    qget: Convert to (url, cbor) prototype.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::io::Read;
+use std::str;
 
 extern crate ureq;
 extern crate serde_cbor;
@@ -8,16 +9,15 @@ use std::collections::HashMap;
 use serde_cbor::{from_slice};
 
 #[no_mangle]
-pub extern "C" fn qget(c: *const c_char) -> *const c_char {
+pub extern "C" fn qget(u: *const c_char, c: *const c_char) -> *const c_char {
   // Build request from CBOR
+  let url = unsafe { CStr::from_ptr(u).to_bytes() };
   let b = unsafe { CStr::from_ptr(c).to_bytes() };
   let v: HashMap<String, String> = from_slice(b).unwrap();
-  let mut url = ureq::get(&v["url"]).build();
-  let mut req: ureq::Request;
-  if v.contains_key("user-agent") {
-    req = url.set("User-Agent", &v["user-agent"]).build();
-  } else {
-    req = url.set("User-Agent", "ureq.qget").build();
+  let mut get = ureq::get(str::from_utf8(url).unwrap()).build();
+  let mut req: ureq::Request = get.set("User-Agent", "ureq.qget").build();
+  for (k, v) in &v {
+    req = get.set(k, v).build();
   }
   // Block!
   let resp = req.call();
